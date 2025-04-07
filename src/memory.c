4e22613b885da1c20254d9168c01864cb06254f9
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-04-07 05:30:32 -0400

    Update memory.c
    I think my previous edits had too much

--- src/memory.c
+++ src/memory.c
@@ -6,13 +6,10 @@ char physical_memory[PHYSICAL_MEMORY_SIZE];
 PageTableEntry page_table[PAGE_TABLE_SIZE];
 TLBEntry tlb[TLB_SIZE];
 
-//to help with page replacement -- acts as a queue
-int pagesUsed [128];
-int framesUsed [128]; //holds values of next_free_frame used for writing
-bool frameWrite [128]; //for each frame number, determines whether it's been written to
-int curFrame = 0; //which index to add into
-int framesFilled = 0; //how many have been written to
-//note that these only include those written to; those not written to can be replaced w/o much fanfare
+//to help with page replacement -- tells us how many are in
+//we add frames in 0, 1, 2... 127 -- thus we don't need a specific queue
+//we can use next_free_frame to tell us how to start
+int framesFilled = 0; //how many have been placed into the table
 
 int tlb_index = 0;
 int next_free_frame = 0;
@@ -32,8 +29,14 @@ int translate_address(uint32_t addr) {
         tlb_hits++;
     } else {
         if (!page_table[page_number].valid) {
+            int curFrame = next_free_frame; //since NFF will be incremented
             frame_number = handle_page_fault(page_number);
+            
             page_faults++;
+            framesFilled++;
+            if (next_free_frame == 128) {
+                next_free_frame = 0;
+            }
         } else {
             frame_number = page_table[page_number].frame_number;
         }
@@ -46,16 +49,7 @@ int translate_address(uint32_t addr) {
     printf("Logical: 0x%04X | Physical: 0x%04X | Value: %d", addr & 0xFFFF, physical_address, value);
 
     if (write_bit) {
-        if (frameWrite[frame_number] == false) { //adding in new "dirty" page
-            framesUsed[curFrame] = frame_number;
-            framesFilled++;
-            pagesUsed[curFrame] = page_number;
-            frameWrite[frame_number] = true;
-            curFrame++;
-            if (curFrame > 127) {
-                curFrame = curFrame-128;
-            }
-        }
+        frameWrite[frame_number] = true;
         physical_memory[physical_address]++;
         page_table[page_number].dirty = 1;
         printf(" | [DIRTY]");
