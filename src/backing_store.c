a00db20542997874728d6f952b8cb5723dd744c0
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-04-07 11:26:22 -0400

    Update backing_store.c

--- src/backing_store.c
+++ src/backing_store.c
@@ -14,6 +14,7 @@ int handle_page_fault(int page_number) {
     if (framesFilled == 128 && frameWrite[next_free_frame]) {
         fwrite(physical_memory + (next_free_frame * FRAME_SIZE), sizeof(char), FRAME_SIZE, bs);
         frameWrite[next_free_frame] = false; //since now not necessarily written
+        page_table[framesToPages[next_free_frame]] = -1; //to show that it's invalid
     } //something written
 
     fseek(bs, page_number * FRAME_SIZE, SEEK_SET);
