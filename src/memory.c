d9e367d370205f0c46d0dffb5ad7524a1bd87309
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-04-07 04:26:54 -0400

    Update memory.c
    Included variables needed for Part 2

--- src/memory.c
+++ src/memory.c
@@ -6,6 +6,12 @@ char physical_memory[PHYSICAL_MEMORY_SIZE];
 PageTableEntry page_table[PAGE_TABLE_SIZE];
 TLBEntry tlb[TLB_SIZE];
 
+//to help with page replacement -- acts as a queue
+int framesUsed [128]; //holds values of next_free_frame used for writing
+bool frameWrite [128]; //for each frame number, determines whether it's been written to
+int curFrame = 0; //which index to add into
+int framesFilled = 0; //how many have been written to
+
 int tlb_index = 0;
 int next_free_frame = 0;
 
@@ -38,6 +44,10 @@ int translate_address(uint32_t addr) {
     printf("Logical: 0x%04X | Physical: 0x%04X | Value: %d", addr & 0xFFFF, physical_address, value);
 
     if (write_bit) {
+        if (frameWrite[frame_number]) {
+            framesUsed[curFrame] = frame_number;
+        }
+        frameWrite[frame_number] = true;
         physical_memory[physical_address]++;
         page_table[page_number].dirty = 1;
         printf(" | [DIRTY]");
