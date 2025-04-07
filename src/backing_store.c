db27b0c173bb11937deb5dc675fbb3bddabd014f
Anthony Duan <123067489+ADuan2003@users.noreply.github.com>
2025-04-07 11:45:48 -0400

    Update backing_store.c
    Accounting for TLB and page table changes

--- src/backing_store.c
+++ src/backing_store.c
@@ -10,16 +10,26 @@ int handle_page_fault(int page_number) {
         exit(1);
     }
 
-    bool full = false;
+    bool TLBreplace = false;
+    int locTLB = 0;
     if (framesFilled == 128 && frameWrite[next_free_frame]) {
         fwrite(physical_memory + (next_free_frame * FRAME_SIZE), sizeof(char), FRAME_SIZE, bs);
         frameWrite[next_free_frame] = false; //since now not necessarily written
-        page_table[framesToPages[next_free_frame]] = -1; //to show that it's invalid
+        page_table[framesToPages[next_free_frame]].valid = 0; //to show that it's invalid
+        //and just in case in TLB...
+        locTLB = search_tlb(framesToPages[next_free_frame]);
+        if (locTLB != -1) {
+            TLBreplace = true;
+        }
     } //something written
 
     fseek(bs, page_number * FRAME_SIZE, SEEK_SET);
     fread(physical_memory + (next_free_frame * FRAME_SIZE), sizeof(char), FRAME_SIZE, bs);
     fclose(bs);
+
+    if (TLBreplace) {
+        tlb[locTLB].page_number = page_number;
+    }
 
     page_table[page_number].frame_number = next_free_frame;
     
